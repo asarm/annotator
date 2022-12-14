2aa1d9d8b10d7a42b0e3ee9c868a596f2ecbac0c
Alperen <ozkayaal@mef.edu.tr>
2022-12-14 05:18:54 +0300

    Implemented components
    - Edit questions
    - Edit answers
    - Update and download data
    - Colorize the answers in context
    - Update the start-end indices of answers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { useState, useRef } from 'react';
 import { useEffect } from 'react';
+import { saveAs} from 'save-as';
+import Highlighter from "react-highlight-words";
+
+
 
 function App() {
   const [jsonData, setJsonData] = useState(null);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [questionAnswers, setQuestionAnswers] = useState([]);
 
+  // create a variable that allows user to upload a JSON file and targets the object "data"
   const handleFileSelect = (event) => {
     const file = event.target.files[0];
 
@@ -20,6 +24,13 @@ function App() {
     }
   };
 
+   // download the updated data in JSON format
+   const handleDownloadData = () => {
+    const file = new Blob([JSON.stringify({data: jsonData}, null, 2)], {type: 'text/plain'});
+    saveAs(file, 'data.json');
+  };
+
+
   const handlePrevClick = () => {
     if (currentIndex > 0) {
       setCurrentIndex(currentIndex - 1);
@@ -32,37 +43,96 @@ function App() {
     }
   };
 
-  const handleAnswerChange = (questionId, newAnswer) => {
-  const updatedQuestionAnswers = questionAnswers.map(qa => {
-    if (qa.id === questionId) {
-      return { ...qa, answer: newAnswer };
-    } else {
-      return qa;
-    }
-  });
-  setQuestionAnswers(updatedQuestionAnswers);
-};
 
-    
+  // allows user to edit the question
+  const changeQuestion = (id) => {
+    const q_input = prompt('Please write your question');
+    const newJsonData = [...jsonData];
+    const text = newJsonData[currentIndex].qas.find(q => q.id === id);
+    text.question = q_input; // update the question
+    setJsonData(newJsonData);
+  };
+
+  // allows user to edit the answer
+  const handleAnswerEdit = (id) => {
+    const a_input = prompt('Enter the answer');
+    const newJsonData = [...jsonData];
+    const text = newJsonData[currentIndex].qas.find(text => text.id === id); // find the corresponding text by id
+    text.answer = a_input; // update the answer
+    setJsonData(newJsonData);
+  };
+
+  
+  // different colors for different answers
+  const colors = ["#ffc107", "#17a2b8", "#28a745", "#dc3545", "#6c757d"];
+  
+  // highlight the answers in the context
+  const colorizeAnswers = (text) => {
+    const answers = jsonData[currentIndex].qas.map((q) => q.answer);
+  
+    // map the answers to the corresponding highlighted answers
+    const highlightedAnswers = answers.map((answer, index) => {
+      // start and end index of the answer
+      const startIndex = text.indexOf(answer);
+      const endIndex = startIndex + answer.length;
+  
+      // Update start and end indices
+      jsonData[currentIndex].qas[index].answer_start = startIndex;
+      jsonData[currentIndex].qas[index].answer_end = endIndex;
+  
+      return (
+        <Highlighter
+          highlightClassName="highlight"
+          searchWords={[answer]}
+          autoEscape={true}
+          textToHighlight={text}
+          highlightStyle={{ backgroundColor: colors[index % colors.length] }}
+        />
+      );
+    });
+  
+    return highlightedAnswers; // TODO: do not return additional contexts
+  };
+  
+  
+
+  
   return (
     <div>
+      
+      <h1>QA Annotation Tool</h1>
+      <p>Upload the data in JSON format</p>
       <input type="file" onChange={handleFileSelect} />
-      {jsonData && (
+      <button onClick={handleDownloadData}>Download Data</button>
+    
+      {jsonData &&  (
         <div style={{margin: 100}}>
+          
           <span>{jsonData[currentIndex].title}</span>
           <p>{jsonData[currentIndex].context}</p>
+          <p>{colorizeAnswers(jsonData[currentIndex].context)}</p>
           <ul>
             {
-            jsonData[currentIndex].qas.map(question => (
+            jsonData[currentIndex].qas.map(text => (
               <li>
-                <p>Question: {question.question}</p>
-                <p>Answer: {question.answer}</p>
+                <p>Question: {text.question} 
+                <button onClick={() => changeQuestion(text.id)}
+                
+                style={{ marginLeft: "15px" }}
+                >Edit</button> </p>
+                
+                <p>Answer: {text.answer} 
+                <button onClick={() => handleAnswerEdit(text.id)}
+                style={{ marginLeft: "15px" }}
+                >Edit</button></p> 
+              
               </li>
             ))
             }
           </ul>
           <button onClick={handlePrevClick}>Previous</button>          
           <button onClick={handleNextClick}>Next</button>
+            
         </div>
       )}
     </div>
@@ -70,4 +140,7 @@ function App() {
 }
 
 
+
+
 export default App;
+
